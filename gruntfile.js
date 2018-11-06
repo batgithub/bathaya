30fd25c9efb1efc706b2f10f6189609ae5b19342
Baptiste <bath12@hotmail.fr>
2018-11-06 22:08:39 +0100

    install jekyll grunt

--- gruntfile.js
+++ gruntfile.js
@@ -8,90 +8,110 @@ module.exports = function(grunt){
 	grunt.initConfig({	//initialisation de l'ensemble des tâches
 
 
-        sass: {                              // Task
-            dev: {                            // Target
-                files: {                         // Dictionary of files
-                    'dev/style.css': 'dev/sass/style.scss'
-                },
-                options: {
-                    update: true,
-                    sourcemap: 'auto'
-
-                }
+    sass: {                              // Task
+        dev: {                            // Target
+            files: {                         // Dictionary of files
+                'dev/style.css': 'dev/sass/style.scss'
             },
-            docs: {                            // Target
-                files: {                         // Dictionary of files
-                    'docs/style.css': 'dev/sass/style.scss'
-                },
-                options: {
-                    update: true,
-                    sourcemap: 'none',
-                    style:'nested'
+            options: {
+                update: true,
+                sourcemap: 'auto'
 
-                }
             }
         },
-        copy: {
-          main: {
-            files: [
-              // includes files within path
-              {
-                expand: true,
-                cwd:'dev',
-                src: ['index.html','src/*'],
-                dest: 'docs/',
-                filter: 'isFile'
-              },
-            ],
-          },
-        },
-        autoprefixer: {
-          docs :{
-              files: {
-                  // Target-specific file lists and/or options go here.
-                  'docs/style.css':'dev/style.css',
-              }
-          }
-        },
-        uglify: {
-          dev: {
+        docs: {
             files: {
-              'dev/app.js': [
-                'dev/js/test1.js',
-                'dev/js/test2.js'
-              ]
-            }
-          },
-          docs: {
-            files: {
-              'docs/app.js': [
-                'dev/js/test1.js',
-                'dev/js/test2.js'
-              ]
-            }
-          }
-        },
+                'docs/style.css': 'dev/sass/style.scss'
+            },
+            options: {
+                update: true,
+                sourcemap: 'none',
+                style:'nested'
 
-        watch: {
-		       options: {
-      		        livereload: true,
-	        },
-            html: {
-                files: ['**/*.html']
-            },
-            sass: {
-                files: ['dev/sass/**/*.scss'],
-                tasks: ['sass:dev'],
-                options: { spawn: false }
-            },
-            js: {
-              files: ['dev/js/**/*.js'],
-              task: ['uglify:dev']
-            },
-            grunt: {
-                files: ['gruntfile.js'],
             }
         }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {
+            expand: true,
+            cwd:'dev',
+            src: ['index.html','src/*'],
+            dest: 'docs/',
+            filter: 'isFile'
+          },
+        ],
+      },
+    },
+    autoprefixer: {
+      docs :{
+          files: {
+              // Target-specific file lists and/or options go here.
+              'docs/style.css':'dev/style.css',
+          }
+      }
+    },
+    uglify: {
+      dev: {
+        files: {
+          'dev/app.js': [
+            'dev/js/test1.js',
+            'dev/js/test2.js'
+          ]
+        }
+      },
+      docs: {
+        files: {
+          'docs/app.js': [
+            'dev/js/test1.js',
+            'dev/js/test2.js'
+          ]
+        }
+      }
+    },
+    jekyll: {                             // Task
+      options: {                          // Universal options
+        bundleExec: true,
+        src : '<%= app %>'
+      },
+      dist: {                             // Target
+        options: {                        // Target options
+          dest: '<%= dist %>',
+          config: '_config.yml,_config.build.yml'
+        }
+      },
+      serve: {                            // Another target
+        options: {
+          serve: true,
+          dest: '.jekyll',
+          drafts: true,
+          future: true
+        }
+      }
+    }
+
+    watch: {
+       options: {
+  		        livereload: true,
+      },
+        html: {
+            files: ['**/*.html']
+        },
+        sass: {
+            files: ['dev/sass/**/*.scss'],
+            tasks: ['sass:dev'],
+            options: { spawn: false }
+        },
+        js: {
+          files: ['dev/js/**/*.js'],
+          task: ['uglify:dev']
+        },
+        grunt: {
+            files: ['gruntfile.js'],
+        }
+    }
 
 	});
 
